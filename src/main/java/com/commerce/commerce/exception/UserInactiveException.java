4df69f77f07ac7f72629cf12bc7ede1dc368abdd
Hamza Ahmyttou <hamza.ahmyttou@outlook.com>
2026-02-04 20:56:18 +0100

    Exception

--- src/main/java/com/commerce/commerce/exception/UserInactiveException.java
+++ src/main/java/com/commerce/commerce/exception/UserInactiveException.java
@@ -1,17 +1,10 @@
 package com.commerce.commerce.exception;
 
-import org.springframework.stereotype.Component;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Component
-@Getter
-@Setter
 public class UserInactiveException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2062752808483825752L;
+
+	public UserInactiveException() {
+		super("User inactive!");
+	}
 }
