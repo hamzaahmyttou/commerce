004be26e9ad573a9c94c846af3f20102f4b93d68
Hamza Ahmyttou <hamza.ahmyttou@outlook.com>
2026-07-24 02:32:53 +0100

    Erase serialVersionUID

--- api/src/main/java/com/commerce/commerce/exception/EmailAlreadyUsedException.java
+++ api/src/main/java/com/commerce/commerce/exception/EmailAlreadyUsedException.java
@@ -2,8 +2,6 @@ package com.commerce.commerce.exception;
 
 public class EmailAlreadyUsedException extends RuntimeException {
 
-	private static final long serialVersionUID = -2062752808483825752L;
-
 	public EmailAlreadyUsedException() {
 		super("Email already used!");
 	}	
--- api/src/main/java/com/commerce/commerce/exception/UserInactiveException.java
+++ api/src/main/java/com/commerce/commerce/exception/UserInactiveException.java
@@ -2,8 +2,6 @@ package com.commerce.commerce.exception;
 
 public class UserInactiveException extends RuntimeException {
 
-	private static final long serialVersionUID = -2062752808483825752L;
-
 	public UserInactiveException() {
 		super("User inactive!");
 	}
